146eda6707cf20229c2a0c592f0b125e38966e5e
amin_chen <minde85731@gmail.com>
2024-09-19 18:07:37 +0800

    新增 Login相關View

--- MVC施工地/Solution0918/Chimo.WebAPI.Site/Models/ViewModels/LoginVm.cs
+++ MVC施工地/Solution0918/Chimo.WebAPI.Site/Models/ViewModels/LoginVm.cs
@@ -8,7 +8,7 @@ namespace Chimo.WebAPI.Site.Models.ViewModels
 {
     public class LoginVm
     {
-        [Display(Name ="用戶名稱")]
+        
         [Required(ErrorMessage ="{0}必填")]
         public string Account {  get; set; }
 
