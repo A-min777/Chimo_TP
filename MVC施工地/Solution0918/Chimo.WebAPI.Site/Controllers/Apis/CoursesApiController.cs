cf92d2d4465d62f1976f9bc8e612c00d643a0c25
TenjouSora <bukifu38@gmail.com>
2024-09-21 05:34:49 +0900

    ...

---
+++ MVC施工地/Solution0918/Chimo.WebAPI.Site/Controllers/Apis/CoursesApiController.cs
@@ -0,0 +1,48 @@
+﻿using Chimo.WebAPI.Site.Models.EFModels;
+using Chimo.WebAPI.Site.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chimo.WebAPI.Site.Controllers.Apis
+{
+	public class CoursesApiController : ApiController
+	{
+		private readonly CourseService _service;
+
+		public CoursesApiController()
+		{
+			_service = new CourseService();
+		}
+	
+
+		[HttpGet]
+		[Route("api/courses/recommended")]
+		public IHttpActionResult GetRecommendedCourses()
+		{
+			try
+			{
+				var courseList = _service.GetRecommendedCourses();
+
+				return Ok(courseList);
+			}
+			
+			catch (Exception ex)
+			{
+				return InternalServerError(ex);
+			}
+		}
+
+		[HttpGet]
+		[Route("api/courses/purchased")]
+		public IHttpActionResult GetPurchasedCourses()
+		{
+			
+
+			return Ok();
+		}
+	}
+}
---
+++ MVC施工地/Solution0918/Chimo.WebAPI.Site/Models/ViewModels/RecommendedCourseVm.cs
@@ -0,0 +1,24 @@
+﻿using Chimo.WebAPI.Site.Models.Dtos;
+using Chimo.WebAPI.Site.Models.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chimo.WebAPI.Site.Models.ViewModels
+{
+	public class RecommendedCourseVm
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public int Price { get; set; }
+
+		public string Thumbnail { get; set; }
+
+		public TeacherDto Teacher { get; set; }
+	}
+}
