6a774f3c12a329619112e0cbe1dd15b5999b35c1
TenjouSora <bukifu38@gmail.com>
2024-09-30 16:30:07 +0800

    0930進度

---
+++ MVC施工地/Solution0918/Chimo.WebAPI.Site/Interfaces/ICartRepository.cs
@@ -0,0 +1,17 @@
+﻿using Chimo.WebAPI.Site.Models.Dtos;
+using Chimo.WebAPI.Site.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chimo.WebAPI.Site.Interfaces
+{
+    public interface ICartRepository
+    {
+        bool AddCartItem(AddCartItemDto dto);
+        List<ShowCartItemDto> GetCartItemsByMemberId(int memberId);
+        bool RemoveCartItem(int cartItemId);
+    }
+}
